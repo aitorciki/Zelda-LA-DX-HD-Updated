08087cb44f2be6e0aff47260b0976d0d5edd8224
BigheadSMZ <Bighead.0@gmail.com>
2026-05-08 18:09:41 -0400

    Patcher: Update publish script and add MacOS codesigning app.
    This updates the publish script to include code signing for the MacOS builds. It uses a personal key generated by "rcodesign.exe" that is not included in the repo, though users can generate their own key and modify the path if they really want to use the publish script to build MacOS versions of the patcher. I don't see this as being very likely since a user can just publish the game and launcher, so they have no need for a patcher. But if I am to ever retire from this project, if the next person wanted to generate patchers like I do they would have to generate a new key.

--- ladxhd_patcher_source_code/publish.bat
+++ ladxhd_patcher_source_code/publish.bat
@@ -1,6 +1,15 @@
 @echo off
-cd /d "%~dp0"
+setlocal enabledelayedexpansion
+
+for %%I in ("%~dp0.") do set "Root=%%~fI"
+cd /d "%Root%"
 Title LADXHD: Patcher Publish Script
+
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+:: Game Version
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+set GameVersion=1.8.1
 
 ::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 :: Clean Previous Builds
@@ -34,6 +43,37 @@ if %errorlevel% neq 0 ( echo MacOS x64 build failed! & pause & exit /b 1 )
 echo Building MacOS Arm64...
 dotnet publish LADXHD_Patcher.csproj -r osx-arm64 /p:PublishProfile=macOS-arm64
 if %errorlevel% neq 0 ( echo MacOS Arm64 build failed! & pause & exit /b 1 )
+
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+:: Rename output "Launcher" executables to "LADXHD.Patcher.v#.#.#".
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+echo.
+echo Renaming output executables...
+cd %cd%\~Publish
+for /r %%f in (Patcher.*) do (
+    if /I "%%~nf"=="Patcher" (
+        echo Renaming: "%%f" to "LADXHD.Patcher.v%GameVersion%%%~xf"
+        ren "%%f" "LADXHD.Patcher.v%GameVersion%%%~xf"
+    )
+)
+
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+:: Sign the MacOS Launcher
+::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+:: Uses a personal signing key. Users building for MacOS would need to generate their own key using "rcodesign.exe". 
+:: This can be done with command: rcodesign generate-self-signed-certificate --person-name NAME > \path\to\NAME.pem
+
+set CodeSignApp="%Root%\Resources\rcodesign.exe"
+set CodeSignKey="%USERPROFILE%\LADXHD\Bighead.pem"
+
+echo.
+echo Signing MacOS-x64 executable...
+%CodeSignApp% sign --pem-source %CodeSignKey% "%Root%\~Publish\macOS-x64\LADXHD.Patcher.v%GameVersion%"
+
+echo.
+echo Signing MacOS-Arm64 executable...
+%CodeSignApp% sign --pem-source %CodeSignKey% "%Root%\~Publish\macOS-arm64\LADXHD.Patcher.v%GameVersion%"
 
 ::───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 :: Finish
